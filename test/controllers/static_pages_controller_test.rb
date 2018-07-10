5ae220027e06f75f2bf8fdf905f3ff67d2b2aa1d
Julian Nicholls <juliannicholls29@gmail.com>
2018-07-10 19:46:11 +0100

    Test layout integration

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,7 +6,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get root_url
+    get root_path
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
