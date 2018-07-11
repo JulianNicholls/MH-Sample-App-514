3216e5e04b045e0e77a8488dc49a40e67d8d8669
Julian Nicholls <juliannicholls29@gmail.com>
2018-07-11 19:57:34 +0100

    Add show user, with gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+  def gravatar_for(user, size: 80)
+    g_id = Digest::MD5::hexdigest(user.email.downcase)
+    g_url = "//www.gravatar.com/avatar/#{g_id}?size=#{size}"
+    image_tag g_url, alt: user.name, class: 'gravatar'
+  end
 end
