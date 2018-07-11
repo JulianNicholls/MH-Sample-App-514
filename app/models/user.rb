083d46fde2184eed9bd42144941da41b25bf0590
Julian Nicholls <juliannicholls29@gmail.com>
2018-07-11 18:53:39 +0100

    Enforce lower-case email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  before_save { email.downcase! }
 
   validates :name, presence: true, length: { in: 3..50 }
 
-  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
   validates :email, 
       presence: true, 
